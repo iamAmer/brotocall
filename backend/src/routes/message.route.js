a71fb9fa0985506a0134d01eb2c6d10bee4263dd
amer <muhammedamer7810@gmail.com>
2025-09-30 15:17:02 +0300

    make ready for deployment

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -3,6 +3,6 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/send', (req, res) => {
-    res.send('Send message endpoint')
-})
-export default router
+  res.send('Send message endpoint');
+});
+export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,29 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import path from 'path';
 
-import authRoutes from "./routes/auth.route.js"
-import messageRouters from "./routes/message.route.js"
+import authRoutes from './routes/auth.route.js';
+import messageRouters from './routes/message.route.js';
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+const __dirname = path.resolve();
 
-app.use('/api/auth', authRoutes)
-app.use('api/message', messageRouters)
+app.use('/api/auth', authRoutes);
+app.use('api/message', messageRouters);
+
+// make ready for deployment
+if (process.env.NODE_ENV == 'production') {
+  console.log(`dirname: ${__dirname}`);
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(colors.yellow(`Server is running on port ${PORT}`));
