fe5f4769de4a7cd42d32c5474179a4d6e1c8f907
amer <muhammedamer7810@gmail.com>
2025-10-27 04:37:27 +0300

    feat: implement frontend of chat page

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,12 +1,35 @@
-import { useAuthStore } from "../store/useAuthStore";
+import { useChatStore } from "../store/useChatStore";
+import ProfileHeader from "../components/ProfileHeader";
+import ActiveTabSwitch from "../components/ActiveTabSwitch";
+import ChatsList from "../components/ChatsList";
+import ContactList from "../components/ContactList";
+import ChatContainer from "../components/ChatContainer";
+import NoConversationPlaceholder from "../components/NoConversationPlaceholder";
 
 function ChatPage() {
-  const { logout } = useAuthStore();
+  const { activeTab, selectedUser } = useChatStore();
+
   return (
-    <div className="z-10">
-      ChatPage
-      <button onClick={logout}>logout</button>
+    <div className="w-full h-screen p-4">
+      <div className="w-full h-full relative z-10">
+        <div className="bg-zinc-950/90 backdrop-blur-sm border border-zinc-800 rounded-lg overflow-hidden shadow-2xl h-full flex">
+          {/* LEFT SIDE */}
+          <div className="w-80 bg-zinc-900/50 flex flex-col border-r border-zinc-800">
+            <ProfileHeader />
+            <ActiveTabSwitch />
+            <div className="flex-1 overflow-y-auto p-4 space-y-2">
+              {activeTab === "chats" ? <ChatsList /> : <ContactList />}
+            </div>
+          </div>
+
+          {/* RIGHT SIDE */}
+          <div className="flex-1 flex flex-col bg-zinc-950/50">
+            {selectedUser ? <ChatContainer /> : <NoConversationPlaceholder />}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
 export default ChatPage;
